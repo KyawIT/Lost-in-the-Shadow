f3fc545eda8644e460829cfc05458cd43f578285
Kyaw Htet Htun <kyawhtethtun65@gmail.com>
2021-06-11 22:14:33 +0200

    Test commit

--- SceneChanger_Start.cs
+++ SceneChanger_Start.cs
@@ -7,7 +7,7 @@ public class SceneChanger : MonoBehaviour
 {
     public void ChangeScene()
     {
-        Debug.Log("Switched to End Scene!");// Debug.Log() is a Console.WriteLine() refrence.
+        Debug.Log("Switched to End Scene!");// Debug.Log() is a Console.WriteLine() refrence
         if (SceneManager.GetActiveScene().buildIndex == 0) // if the build index is 0
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Change to second scene

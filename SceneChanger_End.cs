2eaa16135a016495a0ad860778e65dae41d7f05e
Kyaw Htet Htun <kyawhtethtun65@.com>
2021-06-11 22:56:47 +0200

    Added Comment to Scenechanger_End
    Added Comment to Scenechanger_End

--- SceneChanger_End.cs
+++ SceneChanger_End.cs
@@ -7,6 +7,7 @@ public class SceneChanger_End : MonoBehaviour
 {
     public void ChangeScene_End()
     {
+        // If Player is on Intro Scene and pressed Quit then goto last Scene to Quit
         if (SceneManager.GetActiveScene().buildIndex == 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);

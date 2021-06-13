adb4c5893daf8e65c14af04a72bc92c01dfd8970
Kyaw Htet Htun <kyawhtethtun65@.com>
2021-06-13 15:28:53 +0200

    AddForce added for Jump
    Its need one Jump detector and the player can to move in the air.

--- Lost-In-The-Shadow/Assets/Script/PlayerMovement.cs
+++ Lost-In-The-Shadow/Assets/Script/PlayerMovement.cs
@@ -6,20 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2D;
     public float xVelocitySpeed, jumpForce;
-    float movementInputX;
+    float movementInputX, movementInputY;
     private void Update()
     {
         // Input.GetAxisRaw("Horizontal") = Output is 1 if you press D or right ArrowKey
         // Input.GetAxisRaw("Horizontal") = Output is -1 if you press A or left ArrowKey
         movementInputX = Input.GetAxisRaw("Horizontal");
+        movementInputY = Input.GetAxisRaw("Vertical");
     }
 
     // With FixedUpdate frames are better caculated and we are messing with Physic
     private void FixedUpdate()
     {
         // All that above times movementX so it can move right or left. Example: speed = 100 * movementInputX = -1
-        // 100 * -1 = -100 wihch means its going left hi 
-        Vector3 movement = new Vector3(movementInputX, 0 ,0);
-        transform.position += movement * Time.deltaTime * xVelocitySpeed; 
+        // 100 * -1 = -100 wihch means its going left
+        Vector3 movement = new Vector3(movementInputX, 0, 0);
+        transform.position += movement * Time.deltaTime * xVelocitySpeed;
+        // if movementInputY has the value of 1 its go up else its go down
+        if (movementInputY == 1)
+        {
+            rb2D.AddForce(new Vector2(0, jumpForce) * Time.deltaTime, ForceMode2D.Impulse);
+        }
     }
 }

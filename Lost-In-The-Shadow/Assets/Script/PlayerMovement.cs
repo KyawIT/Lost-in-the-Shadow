0fa99af0780e46ea72480cf6d544394c534cf158
Kyaw Htet Htun <kyawhtethtun65@.com>
2021-06-15 02:13:54 +0200

    Rotation of Player added, a bitmap and tilepalette added to make lvl easier, Charakter PNG added

--- Lost-In-The-Shadow/Assets/Script/PlayerMovement.cs
+++ Lost-In-The-Shadow/Assets/Script/PlayerMovement.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2D;
+    public Transform transform1;
     public float xVelocitySpeed, jumpForce;
     float movementInputX, movementInputY;
+
+    /// <summary>
+    /// With Update Function the Frames are better optimized for Exampel: Controls.
+    /// The Function Checks every frame if we pressed something.
+    /// The better your pc, the better the control.
+    /// </summary>
     private void Update()
     {
         // Input.GetAxisRaw("Horizontal") = Output is 1 if you press D or right ArrowKey
@@ -22,10 +29,20 @@ public class PlayerMovement : MonoBehaviour
         // 100 * -1 = -100 wihch means its going left
         Vector3 movement = new Vector3(movementInputX, 0, 0);
         transform.position += movement * Time.deltaTime * xVelocitySpeed;
-        // if movementInputY has the value of 1 its go up else its go down
-        if (movementInputY == 1)
+        // if movementInputY has the value of 1 or Input.GetButtonDown("Jump") == true its go up
+        if (movementInputY == 1 || Input.GetButtonDown("Jump"))
         {
             rb2D.AddForce(new Vector2(0, jumpForce) * Time.deltaTime, ForceMode2D.Impulse);
+        }
+        // If a Horizontal Key is pressed E.g:A, then rotate the charakter about 180 degree
+        if (movementInputX == -1)
+        {
+            transform1.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        // If a Horizontal Key is pressed E.g:D, then rotate the charakter about 0 degree
+        else if (movementInputX == 1)
+        {
+            transform1.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }

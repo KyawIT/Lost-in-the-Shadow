d24a34318f2f7980def177a59c3e4a52b398f457
nik4on <niklas.trinkl@liwest.at>
2021-06-22 08:39:42 +0200

    UI

---
+++ Lost-In-The-Shadow/Assets/Script/UI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+	[SerializeField]
+	public GameObject button;
+
+	void Start()
+	{
+		gameObject.GetComponent<Button>().;
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+		
+	}
+}
